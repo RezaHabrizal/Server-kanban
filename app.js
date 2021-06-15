22b2cd9d92498da780f928018b44f9f03de7e039
RezaHabrizal <jreza.habrizal@gmail.com>
2021-06-16 01:09:33 +0700

    move to own repo

--- app.js
+++ app.js
@@ -1,22 +1,21 @@
-if (process.env.NODE_ENV !== "production"){
-  require('dotenv').config()
+if (process.env.NODE_ENV !== 'production') {
+	require('dotenv').config()
 }
 const express = require('express')
-const cors = require('cors');
+const cors = require('cors')
 const app = express()
 const port = process.env.PORT || 3000
-const router = require('./routes');
-const errorHandler = require('./middlewares/errHandler');
+const router = require('./routes')
+const errorHandler = require('./middlewares/errHandler')
 
 app.use(cors())
 app.use(express.json())
-app.use(express.urlencoded({extended:true}))
-
+app.use(express.urlencoded({ extended: true }))
 
 app.use(router)
 
 app.use(errorHandler)
 
 app.listen(port, () => {
-  console.log(`Kanban app listening at http://localhost:${port}`)
+	console.log(`Kanban app listening at http://localhost:${port}`)
 })
